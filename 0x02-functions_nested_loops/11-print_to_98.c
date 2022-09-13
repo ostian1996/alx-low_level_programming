b9d5d1198ee22d1724befb3b9a4830ad682ce948
ostian1996 <ostianstephane03@gmail.com>
2022-09-14 00:32:23 +0100

    Update 11-print_to_98  file

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,10 +17,8 @@ void print_to_98(int n)
         {
             if (n == 98)
             {
-                printf("%d", n);
-
+                printf("%d, ", n);
                 printf("\n");
-
                 break;
             }
             else
@@ -35,10 +33,8 @@ void print_to_98(int n)
         {
             if (n == 98)
             {
-                printf("%d", n);
-
+                printf("%d, ", n);
                 printf("\n");
-                
                 break;
             }
             else
