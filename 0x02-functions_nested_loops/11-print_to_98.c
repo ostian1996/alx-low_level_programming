cd9e138c9e01ba06182721b2c0ffd0fea7c405b0
ostian1996 <ostianstephane03@gmail.com>
2022-09-14 00:08:38 +0100

    Update 11-print_to_98  file

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,3 @@
-
 /*
  * File: 11-print_to_98.c
  * Auth: Ostian Stéphane ostian1996
@@ -12,36 +11,36 @@
  */
 void print_to_98(int n)
 {
-if (n <= 98)
-{
-for (; n <= 98; n++)
-{
-if (n == 98)
-{
-printf("%d", n);
-printf("\n");
-break;
-}
-else
-{
-printf("%d, ", n);
-}
-}
-}
-else
-{
-for (; n >= 98; n--)
-{
-if (n == 98)
-{
-printf("%d", n);
-printf("\n");
-break;
-}
-else
-{
-printf("%d, ", n);
-}
-}
-}
+    if (n <= 98)
+    {
+        for (; n <= 98; n++)
+        {
+            if (n == 98)
+            {
+                printf("%d", n);
+                printf("\n");
+                break;
+            }
+            else
+            {
+                printf("%d, ", n);
+            }
+        }
+    }
+    else
+    {
+        for (; n >= 98; n--)
+        {
+            if (n == 98)
+            {
+                printf("%d", n);
+                printf("\n");
+                break;
+            }
+            else
+            {
+                printf("%d, ", n);
+            }
+        }
+    }
 }
