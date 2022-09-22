ff45413fe2518ca19a612a8267fbbdcdc51b7bdc
ostian1996 <ostianstephane03@gmail.com>
2022-09-23 00:33:46 +0100

    Fix: 0x06 pointers arrays strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *  _strcmp - compares if 2 strings
+ *  @s1: destination string
+ *  @s2: source string to be copied
+ *  Return: difference of first characters that are of diff value
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int j;
+
+	for (j = 0; s1[j] != '\0' && s2[j] != '\0'; j++)
+		if (s1[j] != s2[j])
+			return (s1[j] - s2[j]);
+	return (0);
+}
